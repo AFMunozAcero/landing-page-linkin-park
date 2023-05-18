691795d7c604d5ffa5a970005f0d086e0a633eb7
AFMunozAcero <andresfelipeacero99@gmail.com>
2023-05-18 14:49:21 -0500

    [CREATE] nav bar

--- scripts.js
+++ scripts.js
@@ -1,4 +1,4 @@
-ocument.querySelectorAll('nav a').forEach(anchor => {
+document.querySelectorAll('nav a').forEach(anchor => {
     anchor.addEventListener('click', function (e) {
         e.preventDefault();
 
